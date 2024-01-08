70e88733a17252e82989f48f8468e4c117a22ced
rafael <rafaelsouza2017a@gmail.com>
2024-01-08 17:39:31 -0300

    Primeira aula - Teste

---
+++ src/Product.java
@@ -0,0 +1,19 @@
+public class Product {
+    public String name;
+    public double price;
+    public int quantity;
+
+    public double TotalValueInStock(){
+        return price * quantity;
+    }
+    public void AddProducts(int quantity){
+        this.quantity += quantity;
+    }
+    public void RemoveProducts(int quantity) {
+      this.quantity -= quantity;
+
+    }
+    public String toString(){
+        return name +"\n"+price+"\n"+quantity+"\n"+TotalValueInStock();
+    }
+}
